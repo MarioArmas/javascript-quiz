1b692041ac8b1688765d4577e50394dac68d6fee
Mario Armas <mario.r.armas@gmail.com>
2023-08-27 16:29:14 -0600

    Add link on production

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,5 +1,6 @@
 export const getAllQuestions = async () => {
-  const res = await fetch('http://localhost:5173/data.json')
+  const API_URL = 'https://javascript-quiz-two.vercel.app/data.json' || 'http://localhost:5173/data.json'
+  const res = await fetch(API_URL)
   const json = await res.json()
   return json
 }
